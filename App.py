762d4dd22c03ed5af2dea4b6f0056a8553c15b26
Aurchon Datta <aurchon.datta@titus.com>
2020-04-30 15:44:56 -0400

    Implemented creation of new spotify playlist

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
 import json
+import requests
+
+from spotify_credentials import user_id, OAuth_token
 
 
 class PlaylistConverter:
 
     def __init__(self):
-        pass
+        self.user_id = user_id
+        self.OAuth_token = OAuth_token
 
     # get the youtube client
     def youtube_login(self):
@@ -12,7 +16,30 @@ class PlaylistConverter:
 
     # create a new spotify playlist
     def make_spotify_playlist(self):
-        pass
+        request_body = json.dumps({
+            "name": "Youtube Playlist",
+            "description": "This playlist was created from your corresponding Youtube playlist",
+            "public": True
+        })
+        query = "https://api.spotify.com/v1/users/{}/playlists".format(
+            self.user_id)
+
+        bearerToken = "Bearer {}".format(OAuth_token)
+        content_headers = {
+            "Content-Type": "application/json",
+            "Authorization": bearerToken,
+        }
+        response = requests.post(
+            query,
+            data=request_body,
+            headers=content_headers
+        )
+        response_json = response.json()
+        print(response_json)
+
+        # return the playlist id
+        return response_json["id"]
+        # return 0
 
     # get the existing playlist from youtube
     def get_youtube_playist(self):
@@ -25,3 +52,7 @@ class PlaylistConverter:
     # add the list of songs into the spotify playlist
     def add_to_spotify_playlist(self):
         pass
+
+
+newPlaylist = PlaylistConverter()
+print(newPlaylist.make_spotify_playlist())
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # YoutubeToSpotify
+
+Resources used:
+Creating Spotify playlists - https://developer.spotify.com/console/post-playlists/
+
+Searching for a song on Spotify - https://developer.spotify.com/console/get-search-item/
+
+Inspired by:
+https://github.com/TheComeUpCode/SpotifyGeneratePlaylist
+
+Required:
+pip install requests
